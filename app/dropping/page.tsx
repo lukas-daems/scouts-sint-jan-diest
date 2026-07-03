60d82d4d1eacca6e7d9ffe5049eec6923fdf8bcf
lukas-daems <152966218+lukas-daems@users.noreply.github.com>
2026-07-03 12:13:13 +0200

    Add compact dropping page route

---
+++ app/dropping/page.tsx
@@ -0,0 +1,7 @@
+import CompactActionPage from "../components/CompactActionPage";
+
+export const dynamic = "force-dynamic";
+
+export default function DroppingPage() {
+  return <CompactActionPage slug="dropping" />;
+}
